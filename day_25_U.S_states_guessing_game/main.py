ae681cb4943ef7b9f7fb6f6748b6516fe543a9ec
Mat <matonworks@gmail.com>
2026-03-16 17:05:13 +0800

    Add day_25: U.S States Guessing Game

---
+++ day_25_U.S_states_guessing_game/main.py
@@ -0,0 +1,40 @@
+"""
+U.S. States Guessing Game.
+The player guesses state names and they appear on the map.
+"""
+
+import turtle
+import pandas as pd
+
+
+SCREEN = turtle.Screen()
+SCREEN.title("U.S. States Game")
+IMAGE = "blank_states_img.gif"
+SCREEN.addshape(IMAGE)
+map_turtle = turtle.Turtle()
+map_turtle.shape(IMAGE)
+
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = SCREEN.textinput(title=(f"{len(guessed_states)}/50 States Correct"),
+                                    prompt="What's another State's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
